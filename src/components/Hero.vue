4c849ff98266fb2882787445e1c42de6d1ee65d9
Daniel McAssey <daniel.mcassey@gmail.com>
2025-06-10 19:22:53 +0100

    Tweak Hero animation

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,8 +4,9 @@
 
 <template>
     <div class="space-y-20">
-        <div class="text-6xl leading-[1.2] font-bold uppercase text-center text-white px-2">
-            <span class="text-primary-500 bg-slate-900/60 px-2">Experiences</span> that are unbounded from <span class="text-primary-500 font-extrabold">reality</span>.
+        <div class="text-6xl leading-[1.2] font-bold uppercase text-center text-white px-2 relative">
+            <span class="text-primary-500 bg-slate-900/80 px-4">Experiences</span> that are unbounded from <span class="invisible px-10">reality</span>.
+            <div class="drop-shadow-md absolute text-center right-[20%] sm:right-28 md:right-14 lg:right-28 bottom-1 text-primary-400 font-extrabold animate-font-cycle">reality</div>
         </div>
         <div class="font-text text-center text-white bg-slate-900/60 hover:bg-slate-800/60 shadow px-4 py-8">
             <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,18 @@ export default {
                 sans: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
                 text: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                'font-cycle': {
+                    '0%, 100%': { 'font-family': 'Space Grotesk' },
+                    '20%': { 'font-family': 'Arial' },
+                    '40%': { 'font-family': 'Nunito' },
+                    '60%': { 'font-family': 'Times New Roman' },
+                    '80%': { 'font-family': 'Garamond' },
+                },
+            },
+            animation: {
+                'font-cycle': 'font-cycle 2.5s linear infinite',
+            },
         },
     },
 
